adc9bfe43539004bf10038431f115e1acfca4b4b
Cancuen <cancuen.net@gmail.com>
2011-03-29 20:31:00 -0600

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Inventario::Application.config.secret_token = '192611013ef369b30e72c126d23a0c1590e114c8aecc9383286a5168ec70fd082fcbcc54ae27ea0be6f694f685324fcb18db3549798764ade223f98b8c6d1712'
